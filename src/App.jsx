4ea45c33aeb0461c53c2f834813a57972cf8d93b
Nickyedw <egonzalesedwin@gmail.com>
2025-06-14 02:35:59 -0500

    Guardando avance funcional con generación PDF pendiente de probar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
+
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import Cart from "./pages/Cart";
@@ -10,17 +11,16 @@ import DetallePedido from './pages/DetallePedido'; // ✅ nuevo componente
 
 function App() {
   return (
-    <Router> {/* ✅ Agrega el Router aquí para habilitar navegación */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/admin/pedidos" element={<AdminPedidos />} />
-        <Route path="/admin/pedidos/:id" element={<DetallePedido />} /> {/* ✅ nueva ruta */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/admin/pedidos" element={<AdminPedidos />} />
+      <Route path="/admin/pedidos/:id" element={<DetallePedido />} />
+    </Routes>
   );
 }
 
 export default App;
+
