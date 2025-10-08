58b4263ef77b47c56fdb4b737f67ee590dac6d77
Nickyedw <egonzalesedwin@gmail.com>
2025-10-08 17:30:46 -0500

    deploy: ship

--- src/layouts/CartLayout.jsx
+++ src/layouts/CartLayout.jsx
@@ -4,11 +4,15 @@ import CartFab from "../components/CartFab";
 import CartQuickView from "../components/CartQuickView";
 
 export default function CartLayout({ children }) {
+  const openMini = () => {
+    window.dispatchEvent(new CustomEvent("cart:quick:open"));
+  };
+
   return (
     <>
       {children}
+      <CartFab onOpenCart={openMini} />
       <CartQuickView />
-      <CartFab />
     </>
   );
 }
