e8151ccfd60980a80a8e169f68ec01424160af61
Nickyedw <egonzalesedwin@gmail.com>
2025-11-28 00:03:19 -0500

    Update GitHub Pages build (relative base)

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const isGithub = mode === "github";   // si el build es para GitHub Pages
+  const isGithub = mode === "github";
 
   return {
     plugins: [react()],
-    base: isGithub ? "/kokorishop/" : "/",
+
+    // Rutas relativas para que funcione tanto en raíz como en /kokorishop/
+    base: "./",
+
     build: {
+      // GitHub Pages: /docs
+      // Vercel: /dist
       outDir: isGithub ? "docs" : "dist",
     },
   };
